6e8f970dea3d6ac5368c96cc989fedb870f3248a
Anik Chakraborty <canik104@gmail.com>
2024-05-04 21:42:20 +0600

    Added drawer

---
+++ lib/components/Drawer.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:password_manager/configs/colors.dart';
+import 'package:password_manager/controllers/UserController.dart';
+import 'package:password_manager/models/UserModel.dart';
+
+class SideDrawer extends StatefulWidget{
+  const SideDrawer({super.key});
+
+  @override
+  State<SideDrawer> createState() => _SideDrawerState();
+}
+
+class _SideDrawerState extends State<SideDrawer> {
+
+  UserModel? user;
+
+  UserController userCtrl = UserController();
+
+  item(String title, IconData icon){
+    return  Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(icon, color: AppColors.secondary, size: 20),
+          const SizedBox(width: 15),
+          Text(title,
+              style: GoogleFonts.montserrat(
+                  color: AppColors.secondary, fontSize: 20)),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+      return Drawer(
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.transparent,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero
+        ),
+        width: Get.width * 0.7,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 10),
+          child: Column(
+             children: [
+               Container(
+                 margin: const EdgeInsets.only(bottom: 20),
+                 padding: const EdgeInsets.all(30),
+                 decoration: const BoxDecoration(
+                   shape: BoxShape.circle,
+                   color: AppColors.secondary
+                 ),
+                 alignment: Alignment.center,
+                 child: const Icon(FontAwesomeIcons.user, color: AppColors.white, size: 40),
+               ),
+
+               Text("~ ${user?.name ?? ""}  ",style: GoogleFonts.montserrat(fontSize: 20, color: AppColors.secondary, fontWeight: FontWeight.bold),),
+               const SizedBox(height: 30),
+               item("Category", FontAwesomeIcons.layerGroup),
+               item("Group", FontAwesomeIcons.userGroup),
+               item("Lost Device", FontAwesomeIcons.mobile),
+             ],
+          ),
+        ),
+
+    );
+  }
+
+  @override
+  void initState() {
+
+    getUserData();
+
+    super.initState();
+  }
+
+  getUserData() async{
+    user = await userCtrl.getUserDetails();
+    setState(() {});
+  }
+
+}
